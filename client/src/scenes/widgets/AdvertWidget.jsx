c07b058a1a31b8a6fc2b031964a34e37f646ef05
Navdeep khede <nayankhede@gmail.com>
2023-01-05 16:08:50 +0530

    base url cured

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -19,7 +19,7 @@ const AdvertWidget = () => {
        width="100%"
        height="auto"
        alt="advertisment"
-       src={process.env.REACT_APP_BASE_URL`/assets/info4.jpeg`}
+       src={`${process.env.REACT_APP_BASE_URL}/assets/info4.jpeg`}
        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
